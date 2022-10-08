1ed4d401af3519a4c9a4e6fe4389cb6df42ae76e
DeluxeLX <1784900074@qq.com>
2022-10-08 19:14:44 +0800

    DeluxeLX 第三次提交 测试修改文件

--- DeluxeLX-blog/src/test/java/com/deluxe/DeluxeLXBlogTest.java
+++ DeluxeLX-blog/src/test/java/com/deluxe/DeluxeLXBlogTest.java
@@ -3,5 +3,6 @@ package com.deluxe;
 public class DeluxeLXBlogTest {
     public static void main(String[] args) {
         System.out.println("测试一下git工具...");
+        System.out.println("测试一下git工具。。。2");
     }
 }
