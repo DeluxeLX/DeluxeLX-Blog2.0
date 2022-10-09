5336b9202ac6e85cc541c754cfb8de51c1fe042e
DeluxeLX <1784900074@qq.com>
2022-10-09 09:32:20 +0800

    DeluxeLX push test

--- DeluxeLX-blog/src/test/java/com/deluxe/DeluxeLXBlogTest.java
+++ DeluxeLX-blog/src/test/java/com/deluxe/DeluxeLXBlogTest.java
@@ -4,5 +4,6 @@ public class DeluxeLXBlogTest {
     public static void main(String[] args) {
         System.out.println("测试一下git工具...");
         System.out.println("测试一下git工具。。。2");
+        System.out.println("push test");
     }
 }
